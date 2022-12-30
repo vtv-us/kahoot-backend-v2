f278320f11b0a5197c460952500a79261d3edbcf
lntvan1606 <70436051+lntvan166@users.noreply.github.com>
2022-12-30 14:44:41 +0700

    delete question trigger

--- migrations/1005_migrate.up.sql
+++ migrations/1005_migrate.up.sql
@@ -23,7 +23,7 @@ begin
     if old."index" != (select max("index") from "question" where "slide_id" = old."slide_id") then
         update "question" set "index" = "index" - 1 where "slide_id" = old."slide_id" and "index" > old."index";
     end if;
-    return nil;
+    return od;
 end;$$ LANGUAGE plpgsql;
 
 create trigger "auto_decrease_index_when_delete_question" before delete on "question" for each row execute procedure "auto_decrease_index_when_delete_question"();
@@ -33,7 +33,7 @@ begin
     if old."index" != (select max("index") from "answer" where "question_id" = old."question_id") then
         update "answer" set "index" = "index" - 1 where "question_id" = old."question_id" and "index" > old."index";
     end if;
-    return nil;
+    return old;
 end;$$ LANGUAGE plpgsql;
 
 create trigger "auto_decrease_index_when_delete_answer" before delete on "answer" for each row execute procedure "auto_decrease_index_when_delete_answer"();
