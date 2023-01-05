2959bfcf644b95069592f9812be480a8e38058ca
lntvan1606 <70436051+lntvan166@users.noreply.github.com>
2023-01-05 18:09:38 +0700

    drop trigger auto change index

---
+++ migrations/1012_migrate.up.sql
@@ -0,0 +1,4 @@
+drop trigger "auto_decrease_index_when_delete_question" on "question";
+drop trigger "auto_decrease_index_when_delete_answer" on "answer";
+drop function "auto_decrease_index_when_delete_question";
+drop function "auto_decrease_index_when_delete_answer";
